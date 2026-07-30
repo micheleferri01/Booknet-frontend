97f91fc4189c43afc517b55b485d8f2fe234468d
micheleferri01 <micheleferri095@gmail.com>
2026-07-30 19:46:48 +0200

    add: imports to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+
+// bootstrap imports
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+// custom css
+import './assets/css/App.css';
+import "./assets/css/searchbar.css";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
