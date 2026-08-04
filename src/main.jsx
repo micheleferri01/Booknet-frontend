f470b1148b48ad82db475fd74262f992afc5ca52
micheleferri01 <micheleferri095@gmail.com>
2026-08-04 22:20:30 +0200

    imported bootstrap-icons

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 // bootstrap imports
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
