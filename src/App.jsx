988f60cec2b270a1e9b1ba300e7fd3858859e128
micheleferri01 <micheleferri095@gmail.com>
2026-08-04 23:16:06 +0200

    add: new route and contexts

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
 import DefaultLayout from './layouts/DefaultLayout';
 import BooksHomePage from './pages/BooksHomePage';
+import BookDetailsPage from './pages/BookDetailsPage';
+import { CartProvider } from './contexts/CartContext';
+import { NotificationContextProvider } from './contexts/NotificationContext';
+import Cart from './pages/Cart';
 
 
 export default function App() {
 
   return (
     <>
-   
-    <BrowserRouter>
-      <Routes>
-        <Route Component={DefaultLayout}>
-          <Route path='/' Component={BooksHomePage}/>
-        </Route>
+    <NotificationContextProvider>
+      <CartProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route Component={DefaultLayout}>
+              <Route path='/' Component={BooksHomePage}/>
+              <Route path='/:slug' Component={BookDetailsPage}/>
+              <Route path='/cart' Component={Cart}/>
+            </Route>
 
-      </Routes>
-    </BrowserRouter>
-
+          </Routes>
+        </BrowserRouter>
+      </CartProvider>
+    </NotificationContextProvider>
     </>
   )
 }
